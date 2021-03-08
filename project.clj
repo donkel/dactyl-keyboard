b68b9383107253a31e049fb5edc2e5cd19719c6b
toti <tmaregge@gmail.com>
2021-03-08 22:23:06 +0100

    Downgraded to 1.8.0
    Got weird errors with 1.10.1.
    Need >= 1.8 for cider nrepl to work.
    Seems to work now.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :main dactyl-keyboard.dactyl
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.4.0"]])
 

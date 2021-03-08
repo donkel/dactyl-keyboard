33aadd53575f5bcb76400609c6e2e5d4d7839df4
toti <tmaregge@gmail.com>
2021-03-08 19:23:02 +0100

    Updated Clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :main dactyl-keyboard.dactyl
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.4.0"]])
 
